96687bcaa5277231769dcdd5694cf4951ef2eeee
Brandon Harris <brandon@relentnet.com>
2025-06-06 22:41:11 -0500

    I have made a breakthrough with iframes.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    web_accessible_resources: [
+      {
+        resources: ['iframe.html'],
+        matches: ['<all_urls>'],
+      },
+    ],
+  },
   modules: ["@wxt-dev/module-react"],
   srcDir: 'src',
   vite: () => ({
     plugins: [tailwindcss()],
   }),
+
 });
