47f87545724deba6724f98e9163c0611b0cea759
Brandon Harris <brandon@relentnet.com>
2025-06-26 15:41:37 -0500

    Added a script to renew SSL certs.

---
+++ backend/renew-certs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /root/scrollr/backend
+docker run --rm \
+  -v $(pwd)/certbot/conf:/etc/letsencrypt \
+  -v $(pwd)/certbot/www:/var/www/certbot \
+  certbot/certbot renew --quiet
+docker-compose restart nginx
